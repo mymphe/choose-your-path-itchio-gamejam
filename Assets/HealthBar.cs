2157caa999c340c4749fff3cf87b0787d3e179c0
mymphe <azatsdev@gmail.com>
2024-05-01 20:08:19 +0500

    health bar prototype

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -3,31 +3,50 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Image[] healthIcons;
+    [SerializeField] private Image filledinHeart;
+    [SerializeField] private Image emptyHeart;
+
     private int maxHealth = 5;
     private int currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
-        UpdateHealthBar();
+        UpdateHealthBar2();
     }
 
-    public void TakeDamage(int damage)
+    public void TakeDamage()
     {
-        currentHealth -= damage;
+        currentHealth -= 1;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-        UpdateHealthBar();
+        UpdateHealthBar2();
     }
 
-    void UpdateHealthBar()
+    void UpdateHealthBar2()
     {
-        for (int i = 0; i < maxHealth; i++)
+        for (var i = gameObject.transform.childCount - 1; i >= 0; i--)
         {
-            if (i < currentHealth)
-                healthIcons[i].enabled = true;
-            else
-                healthIcons[i].enabled = false;
+            Object.Destroy(gameObject.transform.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < currentHealth; i++) {
+            Image heart = Instantiate(filledinHeart, transform);
+        }
+
+        for (int i = 0; i < maxHealth - currentHealth; i++)
+        {
+            Instantiate(emptyHeart, transform);
         }
     }
+
+    //void UpdateHealthBar()
+    //{
+    //    for (int i = 0; i < maxHealth; i++)
+    //    {
+    //        if (i < currentHealth)
+    //            healthIcons[i].enabled = true;
+    //        else
+    //            healthIcons[i].enabled = false;
+    //    }
+    //}
 }
